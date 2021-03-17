d92bcbfb4c5b4b911a404e590335942d6ebc4c6b
spalmeiro <sergio.palmeiro.sanchez@gmail.com>
2021-03-17 20:49:58 +0100

    Cambios footer.html y runserver.bat

--- runserver.bat
+++ runserver.bat
@@ -1,3 +1,3 @@
-cd C:\Users\Usuario\Desktop\Informatica\Programacion\Proyectos\PGAA-Ajedrez\PGAA-Ajedrez-main
-C:\Users\Usuario\Desktop\Informatica\Programacion\Proyectos\PGAA-Ajedrez\PGAA-Ajedrez-venv\Scripts\python.exe manage.py runserver
+cd C:\Users\Usuario\Desktop\Informatica\Programacion\Proyectos\PGAA\AeroChess-main
+C:\Users\Usuario\Desktop\Informatica\Programacion\Proyectos\PGAA\AeroChess-venv\Scripts\python.exe manage.py runserver
 cmd /k
