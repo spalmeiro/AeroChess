13badecbfe2d815a8151db3899764877d32d1281
spalmeiro <sergio.palmeiro.sanchez@gmail.com>
2021-03-17 21:24:19 +0100

    Update runserver.bat

--- runserver.bat
+++ runserver.bat
@@ -1,3 +1,3 @@
-cd C:\Users\Usuario\Desktop\Informatica\Programacion\Proyectos\PGAA\AeroChess-main
-C:\Users\Usuario\Desktop\Informatica\Programacion\Proyectos\PGAA\AeroChess-venv\Scripts\python.exe manage.py runserver
+cd /d %~dp0
+..\AeroChess-venv\Scripts\python.exe manage.py runserver
 cmd /k
