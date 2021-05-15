271712f907b31c6ff9dceb05b94a4cc8f42d21b2
spalmeiro <sergio.palmeiro.sanchez@gmail.com>
2021-05-15 11:24:58 +0200

    Update settings and asgi

--- aerochess/asgi.py
+++ aerochess/asgi.py
@@ -7,22 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
-import django
 import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aerochess.settings')
+django_asgi_app  = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 
 import play.routing
 import puzzles.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aerochess.settings')
-
-django.setup()
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             play.routing.websocket_urlpatterns + puzzles.routing.websocket_urlpatterns
